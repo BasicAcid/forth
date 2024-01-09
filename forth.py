644b488be66a08d4bcfd7f1757734de900762175
BasicAcid <david.tabarie@gmail.com>
2024-01-10 00:57:29 +0100

    Add commons operations.

--- forth.py
+++ forth.py
@@ -17,6 +17,33 @@ def execute(input_str):
                 operand1 = stack.pop()
                 result = operand1 + operand2
                 stack.append(result)
+        elif word == '-':
+            if len(stack) < 2:
+                print("Error: Not enough operands for addition.")
+                return
+            else:
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+                result = operand1 - operand2
+                stack.append(result)
+        elif word == '*':
+            if len(stack) < 2:
+                print("Error: Not enough operands for addition.")
+                return
+            else:
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+                result = operand1 * operand2
+                stack.append(result)
+        elif word == '/':
+            if len(stack) < 2:
+                print("Error: Not enough operands for addition.")
+                return
+            else:
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+                result = operand1 / operand2
+                stack.append(result)
 
     if stack:
         print("Result:", stack[-1])
@@ -24,3 +51,6 @@ def execute(input_str):
         print("Stack is empty.")
 
 execute("5 3 +")
+execute("5 3 -")
+execute("5 3 *")
+execute("5 3 /")
