3052f4dbe1d854fd3038bcf21798859fbf1fa8d4
BasicAcid <david.tabarie@gmail.com>
2024-02-02 13:10:46 +0100

    Add split input by spaces.

--- forth.c
+++ forth.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX_INPUT_SIZE 256 // Okay for now I guess.
 
-void execute(char *input_str) {
-    printf("Executing: %s\n", input_str);
+void execute(char *input_str, size_t str_size)
+{
+    /* for(size_t i = 0; i < str_size; ++i) */
+    /*     printf("%c" , input_str[i]); */
+
+    char *token, *str_copy;
+
+    // Copy the string just in case, as it will be modified by strstep.
+    str_copy = strdup(input_str);
+
+    while((token = strsep(&input_str, " ")))
+        printf("%s" , token);
+
+    free(str_copy);
 }
 
-int main() {
-
+int
+main()
+{
     char input_str[MAX_INPUT_SIZE];
 
     while(1)
@@ -20,7 +34,7 @@ int main() {
         if(strcasecmp(input_str, "quit") == 0)
             break;
 
-        execute(input_str);
+        execute(input_str, strlen(input_str));
     }
 
     return 0;
